487c4f12636045dad9d1a4d3a20c55920ad8eac7
Distinct Wind <307532500@qq.com>
2023-06-02 02:22:43 +0000

    change to expected

--- test.cpp
+++ test.cpp
@@ -1,15 +1,31 @@
-#include <pistache/endpoint.h>
+#include <iostream>
+#include <expected>
+#include <stdexcept>
+#include <string>
+#include <cctype>
+using namespace std;
 
-using namespace Pistache;
-
-struct HelloHandler: 
-    public Http::Handler {
-        HTTP_PROTOTYPE(HelloHandler)
-        void onRequest(const Http::Request&, Http::ResponseWriter writer) override {
-            writer.send(Http::Code::Ok, "Hello, World!");
+expected<int, runtime_error> string_to_int(const string& str) {
+    int result = 0;
+    for (int i = 0; i<str.size(); i++) {
+        char current_char = str[i];
+        if (!isdigit(current_char)) {
+            return unexpected{runtime_error{"Detected non-digit char"}};
         }
-    };
+        int current_digit = current_char - '0';
+        result = result*10 + current_digit;
+    }
+    return result;
+}
 
-int main() {
-    Http::listenAndServe<HelloHandler>(Pistache::Address("*:9080"));
+int main(){
+    // cout<<string_to_int("1246")<<endl;
+    auto result = string_to_int("1234");
+    cout << boolalpha << result.has_value() << endl;
+    if (result.has_value()){
+        cout << result.value() << endl;
+    }
+    else {
+        cout << result.error().what() << endl;
+    }
 }
