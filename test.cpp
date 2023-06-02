8950e750f8065d7269fe09b9f7f349b1042c2cba
Distinct Wind <307532500@qq.com>
2023-06-02 01:31:31 +0000

    Make Pistache example

--- test.cpp
+++ test.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
-#include <version>
-using namespace std;
-int main()
-{
-    cout << __cpp_lib_expected << endl;
+#include <pistache/endpoint.h>
+
+using namespace Pistache;
+
+struct HelloHandler: 
+    public Http::Handler {
+        HTTP_PROTOTYPE(HelloHandler)
+        void onRequest(const Http::Request&, Http::ResponseWriter writer) override {
+            writer.send(Http::Code::Ok, "Hello, World!");
+        }
+    };
+
+int main() {
+    Http::listenAndServe<HelloHandler>(Pistache::Address("*:9080"));
 }
